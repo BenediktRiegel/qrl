db866b78a84302f0b0250f71a476585c510f0f16
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2023-07-13 21:50:50 +0200

    Added configs. Holes now give 0 reward

---
+++ pennylane_implementation/load_config.py
@@ -0,0 +1,126 @@
+from typing import Generator, List, Tuple
+from pathlib import Path
+from json import load, dump
+from environment.frozen_lake import FrozenField
+from datetime import datetime
+from copy import deepcopy
+
+
+default_values = dict(
+    num_iterations=12,
+    # 1: action, 2: value, 3: return both, 4: lam * action + value
+    sub_iterations=[(50, 2), (50, 1)],
+    precise=False,
+    end_state_values=False,
+    action_qnn_depth=1,
+    value_qnn_depth=1,
+    value_optimizer="Adam",
+    action_optimizer="Adam",
+    value_lr=0.5,
+    action_lr=0.5,
+    default_reward=0.0,
+    gamma=0.8,
+    eps=0.0,
+    lam=0.8,
+    backend="pennylane_lightning.kokkos",
+    shots=100000,
+    action_diff_method="best",  # best, adjoint, parameter-shift
+    value_diff_method="best",   # best, adjoint, parameter-shift
+    slip_probabilities=[1. / 3., 1. / 3., 0., 1. / 3.],
+    map=[
+        ["I", "I"],
+        ["H", "G"],
+    ],
+    output_path="./results/",
+)
+
+
+def str_to_map_row(row: str):
+    new_row = []
+    i = 0
+    while i < len(row):
+        if row[i] == "(":
+            j = i + 1
+            while row[j] != ")":
+                j += 1
+            el = row[i + 1:j].split(",")
+            new_row.append((el[0].strip(), el[1].strip()))
+
+            i = j
+
+        elif row[i].lower() == "h":
+            new_row.append(row[i])
+        elif row[i].lower() == "g" or row[i].lower() == "e":
+            new_row.append(row[i])
+        elif row[i].lower() == "i":
+            new_row.append(row[i])
+
+        i += 1
+
+    return new_row
+
+
+def load_map(config_map: List[List] | str) -> List[List[FrozenField]]:
+    if isinstance(config_map, str):
+        map_list = []
+        rows = config_map.split("\n")
+        for row in rows:
+            row = row.strip()
+            if row != "":
+                map_list.append(str_to_map_row(row))
+    frozen_lake = []
+    for row in config_map:
+        new_row = []
+        for element in row:
+            if element.lower() == "h":
+                new_row.append(FrozenField.get_hole())
+            elif element.lower() == "g" or element.lower() == "e":
+                new_row.append(FrozenField.get_end())
+            elif element.lower() == "i":
+                new_row.append(FrozenField.get_ice())
+            else:
+                new_row.append(FrozenField(
+                    reward=float(element[0]),
+                    end=(element[1] or element[1] == 1)
+                ))
+        frozen_lake.append(new_row)
+    return frozen_lake
+
+
+def _load_config(file_path: Path) -> dict:
+    config = default_values
+    old_file_dir = file_path.parent.resolve()
+    with file_path.open("r") as f:
+        config.update(load(f))
+        f.close()
+
+    datetime_str = datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
+    output_dir = Path(config["output_path"]) / datetime_str
+    config["output_dir"] = str(output_dir.resolve())
+
+    return config
+
+
+def load_config(config_path: str | Path) -> Generator[Tuple[Path, dict], None, None]:
+    config_path = Path(config_path)
+
+    if config_path.is_file():
+        files = [config_path]
+    else:
+        files = [x for x in config_path.glob("**/*") if x.is_file() and str(x).lower().endswith(".json")]
+
+    for f in files:
+        yield f, _load_config(f)
+
+
+def save_config(config_path: str | Path, config: dict):
+    if isinstance(config_path, str):
+        config_path = Path(config_path)
+    with config_path.open("w") as f:
+        dump(config, f)
+        f.close()
+
+
+if __name__ == "__main__":
+    path = Path("./configs/default_config.json")
+    save_config(path, default_values)
