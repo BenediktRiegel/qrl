b7be8beb1bfc6ef2775add9ff546fe6c50947ec2
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2023-10-20 16:03:59 +0200

    Added matplotlib option for fourier plot

--- fourier_stuff/plot.py
+++ fourier_stuff/plot.py
@@ -1,4 +1,6 @@
 import plotly.graph_objects as go
+import seaborn as sns
+from numpy import flip
 
 
 def get_fig(x, y, z):
@@ -22,3 +24,15 @@ def get_heatmap(x, y, z):
         x=x,
         y=y,
         hoverongaps=False))
+
+
+def get_matplotlib_heatmap(x, y, z, xlabel, ylabel):
+    z = flip(z, axis=0)
+    plot = sns.heatmap(z, xticklabels=x, linewidths=0.0, rasterized=True)
+    plot.set(xlabel=xlabel, ylabel=ylabel)
+
+    yticklabels = ["-1", "-0.75", "-0.5", "-0.25", "0", "0.25", "0.5", "0.75", "1"]
+    yticks = [len(y), 7*(len(y)+1)//8, 3*(len(y)+1)//4, 5*(len(y)+1)//8, (len(y)+1)//2, 3*(len(y)+1)//8, (len(y)+1)//4, (len(y)+1)//8, 0]
+    plot.set_yticks(yticks)
+    plot.set_yticklabels(yticklabels, rotation=0)
+    return plot
